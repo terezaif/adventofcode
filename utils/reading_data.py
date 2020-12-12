3ec3759ff6a6cfb20ee904c02a54aa2ae800f627
Tereza Iofciu <tereza@neuefische.com>
2020-12-12 12:24:44 +0100

    needed more reading rules

--- utils/reading_data.py
+++ utils/reading_data.py
@@ -1,15 +1,55 @@
-def get_int_input_array(path: str)->list:
+import re
+
+
+def get_int_input_array(path: str) -> list:
     numbers = []
-    with open(path, 'r') as reader:
-    # Read and print the entire file line by line
-     for line in reader:
-        numbers.append(int(line.strip()))
+    with open(path, "r") as reader:
+        # Read and print the entire file line by line
+        for line in reader:
+            numbers.append(int(line.strip()))
     return numbers
 
-def get_string_input_array(path: str)->list:
+
+def get_string_input_array(path: str) -> list:
     strings = []
-    with open(path, 'r') as reader:
-    # Read and print the entire file line by line
-     for line in reader:
-        strings.append(line.strip())
+    with open(path, "r") as reader:
+        # Read and print the entire file line by line
+        for line in reader:
+            strings.append(line.strip())
+    return strings
+
+
+def get_string_input_matrix(path: str) -> list:
+    strings = []
+    with open(path, "r") as reader:
+        # Read and print the entire file line by line
+        for line in reader:
+            strings.append(list(line.strip()))
+    return strings
+
+
+def get_string_input_matrix_w_padding(path: str) -> list:
+    strings = []
+    with open(path, "r") as reader:
+        # Read and print the entire file line by line
+        for line in reader:
+            line = list(line.strip())
+            line.append("-")
+            line.insert(0, "-")
+            strings.append(line)
+
+    length = len(strings[0])
+    li = ["-"] * length
+    strings.insert(0, li)
+    strings.append(li)
+    return strings
+
+
+def get_string_input_matrix_r(path: str, regex: str) -> list:
+    strings = []
+    with open(path, "r") as reader:
+        # Read and print the entire file line by line
+        for line in reader:
+            elems = list(re.findall(regex, line.strip())[0])
+            strings.append(elems)
     return strings
