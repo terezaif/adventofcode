93c402acfbab352a5bf2b512f028a9ee79809ba1
Tereza Iofciu <tereza@neuefische.com>
2021-12-03 08:49:37 +0100

    updating stats

---
+++ stats/parse_code_2021.py
@@ -0,0 +1,54 @@
+from parse import parse
+from pathlib import Path
+import glob
+import pandas as pd
+
+YEAR = 2021
+AUTHOR = "t"
+PATHPATTERN = "2021/*.py"
+PARSEPAT = "2021/{:d}.py"
+
+
+def get_filenames(pathpattern: str = PATHPATTERN) -> list:
+    mdfiles = []
+    for file in glob.glob(pathpattern):
+        mdfiles.append(file)
+    return mdfiles
+
+
+files = get_filenames()
+
+file_stats = []
+
+for filen in files:
+    stats = {}
+    stats["size"] = Path(filen).stat().st_size
+    print(filen)
+
+    p = parse(PARSEPAT, filen)
+    if p is None:
+        continue
+    stats["day"] = p[0]
+    with open(filen, "r") as myfile:
+        data = myfile.read()
+    collect = [
+        "for",
+        "while",
+        "if",
+        "elif",
+        "def",
+        "[",
+        "{",
+        "(",
+        "list",
+        "set",
+        "lambda",
+        "map",
+    ]
+    for st in collect:
+        stats[st] = data.count(st)
+    stats["lines"] = len(data.split("\n"))
+    file_stats.append(stats)
+
+df = pd.DataFrame(file_stats)
+df.to_csv(f"stats/{YEAR}_{AUTHOR}.csv")
