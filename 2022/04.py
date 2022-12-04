3bf64f81450d9b5c440b1150dd348609a7680403
Tereza Iofciu <tereza@macbook>
2022-12-04 09:52:10 +0100

    day 4

--- 2022/04.py
+++ 2022/04.py
@@ -3,22 +3,33 @@ import re
 
 
 def part1(data):
-    lists = [[int(n) for n in re.split(',|-',s)] for s in data.split("\n")]
-    diffs = [(l[0]-l[2])*(l[1]-l[3])<=0 for l in lists]
+    lists = [[int(n) for n in re.split(",|-", s)] for s in data.split("\n")]
+    diffs = [(li[0] - li[2]) * (li[1] - li[3]) <= 0 for li in lists]
     return sum(diffs)
 
 
 def part2(data):
-    lists = [[int(n) for n in re.split(',|-',s)] for s in data.split("\n")]
-    ds = [any([l[0]<=l[2]<=l[1],l[0]<=l[3]<=l[1],l[2]<=l[0]<=l[3],l[2]<=l[1]<=l[3]]) for l in lists]
+    lists = [[int(n) for n in re.split(",|-", s)] for s in data.split("\n")]
+    ds = [
+        any(
+            [
+                li[0] <= li[2] <= li[1],
+                li[0] <= li[3] <= li[1],
+                li[2] <= li[0] <= li[3],
+                li[2] <= li[1] <= li[3],
+            ]
+        )
+        for li in lists
+    ]
     return sum(ds)
+
 
 def test_part1():
     assert part1(test_data) == 2
 
 
 def test_part2():
-    assert part2(test_data) == 10
+    assert part2(test_data) == 4
 
 
 data = get_data(day=4, year=2022)
@@ -32,5 +43,4 @@ test_data = """2-4,6-8
 2-8,3-7
 6-6,4-6
 2-6,4-8
-4-4,5-5
-85-93,39-84"""
+4-4,5-5"""
