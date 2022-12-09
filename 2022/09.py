bd466519ecc7850b5fd4eec4d1232c53df87483c
Tereza Iofciu <tereza@macbook>
2022-12-09 21:11:28 +0100

    fixed bug, cleaned up

--- 2022/09.py
+++ 2022/09.py
@@ -4,49 +4,30 @@ xinc = {"R": 1, "L": -1, "U": 0, "D": 0}
 yinc = {"R": 0, "L": 0, "U": -1, "D": 1}
 
 
-def part1(data):
+def part2(data, r):
     input = [s.split(" ") for s in data.split("\n")]
-    h = [5, 0]
-    t = [5, 0]
-    pos = set()
-    pos.add(tuple(t))
-    for [d, n] in input:
-        print(d, n)
-        n = int(n)
-        while n > 0:
-            prevh = h.copy()
-            h[0] += yinc[d]
-            h[1] += xinc[d]
-            if abs(h[1] - t[1]) > 1 or abs(h[0] - t[0]) > 1:
-                t = prevh
-                pos.add(tuple(t))
-            n -= 1
-    return len(pos)
-
-
-def part2(data, n):
-    input = [s.split(" ") for s in data.split("\n")]
-    rx = [0] * n
-    ry = [5] * n
+    rx = [0] * r
+    ry = [5] * r
     pos = set()
     pos.add((ry[-1], rx[-1]))
     for [d, n] in input:
-        n = int(n)
-        while n > 0:
+        for _ in range(int(n)):
             rx[0] += xinc[d]
             ry[0] += yinc[d]
             for i in range(1, len(rx)):
-                if abs(rx[i] - rx[i - 1]) + abs(ry[i] - ry[i - 1]) >= 3:
-                    rx[i] = rx[i] + int((rx[i - 1] - rx[i]) / abs(rx[i] - rx[i - 1]))
-                    ry[i] = ry[i] + int((ry[i - 1] - ry[i]) / abs(ry[i] - ry[i - 1]))
+                _x = rx[i - 1] - rx[i]
+                _y = ry[i - 1] - ry[i]
+                if abs(_x) + abs(_y) >= 3:
+                    rx[i] += _x / abs(_x)
+                    ry[i] += _y / abs(_y)
                     pos.add((ry[-1], rx[-1]))
-                if abs(rx[i] - rx[i - 1]) == 2:
-                    rx[i] = rx[i] + int((rx[i - 1] - rx[i]) / 2)
+                elif abs(_x) == 2:
+                    rx[i] += (_x) // 2
                     pos.add((ry[-1], rx[-1]))
-                elif abs(ry[i] - ry[i - 1]) == 2:
-                    ry[i] = ry[i] + int((ry[i - 1] - ry[i]) / 2)
+                elif abs(_y) == 2:
+                    ry[i] += (_y) // 2
                     pos.add((ry[-1], rx[-1]))
-            n -= 1
+
     return len(pos)
 
 
